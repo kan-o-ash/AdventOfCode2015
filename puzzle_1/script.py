3d582dc670aea6e8445d4509ddd35cc62e07fa29
Ashkan <ashkan.pk@gmail.com>
2015-12-02 03:00:53 -0500

    Puzzle 1, part 2 complete.

--- puzzle_1/script.py
+++ puzzle_1/script.py
@@ -1,3 +1,4 @@
+######################## PART 1 #####################
 # Santa needs to know which floor of a building to go to
 # input.txt contains directions
 # '(' = one floor up
@@ -8,14 +9,33 @@ def OpenFile(file_name):
     return f.read()
 
 def FindFloor(file_name):
-    string_input = OpenFile(file_name)
-    count = 0
-    for char in string_input:
-        if char == "(":
-            count += 1
-        elif char == ")":
-            count -= 1
+    input_txt = OpenFile(file_name)
+    floor_num = 0
 
-    return count
+    for char in input_txt:
+        if char == "(":
+            floor_num += 1
+        elif char == ")":
+            floor_num -= 1
+
+    return floor_num
     
 print FindFloor('input.txt')
+
+######################## PART 2 #####################
+# Find out how many moves it takes to get to the target floor
+
+def NumMovesToFloor(file_name, floor_target):
+    input_txt = OpenFile(file_name)
+    floor_num = 0
+
+    for i in range(len(input_txt)):
+        ch = input_txt[i]
+        if ch == "(":
+            floor_num += 1
+        elif ch == ")":
+            floor_num -= 1
+        if floor_num == floor_target:
+            return i + 1
+
+print NumMovesToFloor('input.txt', -1)
